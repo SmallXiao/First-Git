f712168c9e06e5a60bf304609289f281fe7afffa
SmallXiao <sunxiao_45@sina.com>
2016-01-27 17:20:17 +0800

    哈哈哈

--- testGit/src/test/java/com/Test/Test1.java
+++ testGit/src/test/java/com/Test/Test1.java
@@ -5,7 +5,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hello!");
 		System.out.println("test");
-		System.out.println("haha");
 		System.out.println("xixi");
+	}
+	
+	public void test(){
+		System.out.println("试试怎么合并！");
 	}
 }
