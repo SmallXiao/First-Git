d2711cd137b059769dfc969100cd6bdbc4befc0c
SmallXiao <sunxiao_45@sina.com>
2016-01-27 17:43:51 +0800

    测试了

--- testGit/src/test/java/com/Test/Test1.java
+++ testGit/src/test/java/com/Test/Test1.java
@@ -6,9 +6,16 @@ public class Test1 {
 		System.out.println("hello!");
 		System.out.println("test");
 		System.out.println("xixi");
+		System.out.println("haha");
 	}
 	
+	/**
+	 * @author voney
+	 * 
+	 */
 	public void test(){
+		
+		System.out.println("测试合并操作");
 		
 	}
 }
